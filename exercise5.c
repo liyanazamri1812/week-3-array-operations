d35ce8023997a2114ab7fe105165a18c89cb1c73
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-06 00:01:14 +0800

    Update exercise5.c

--- exercise5.c
+++ exercise5.c
@@ -1,4 +1,5 @@
-//documentation section 
+//documentation section
+//Array Week 3, Exercise 5
 
 //pre-processor section 
 #include<stdio.h>
@@ -9,7 +10,11 @@
 //main function section 
 int main(){
     //write here your program
-
+    
+    char myWord[50]={"Practice Makes Perfect!"};
+    
+    printf("%s \n" ,myWord);
+    printf("8-th element from the string is: %c" ,myWord[7]);
 
     return 0;
 }
