6c86b9d9360863f22f7566c0a85d776b4c791ef6
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-05 23:16:10 +0800

    Update exercise4.c

--- exercise4.c
+++ exercise4.c
@@ -1,4 +1,5 @@
 //documentation section 
+//Array Week 3 , exercise 4
 
 //pre-processor section 
 #include<stdio.h>
@@ -9,10 +10,21 @@
 //main function section 
 int main(){
     //write here your program
+    
+    float num[]={10.0, 22.5, 15.5, 32.5, 10.9, 67.9, 12.5, 11.9, 25.5, 100.0};
+    float sum=0;
+    int i;
+    
+    for(int i=0; i<10; i++)
+    {
+        sum+=num[i];
+    }
+    
+    printf("%f.2f",sum);
 
 
     return 0;
 }
 
 
-//user-defined section 
+//user-defined section
