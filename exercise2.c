d394cc3bb7582c0fa50cd817d99ee2a0e4b53e2c
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-05 22:20:29 +0800

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -1,4 +1,5 @@
-//documentation section 
+//documentation section
+//Array Week 3 , Exercise 2 
 
 //pre-processor section 
 #include<stdio.h>
@@ -9,10 +10,15 @@
 //main function section 
 int main(){
     //write here your program
+    
+    char myString[100];
+    printf ("Enter Your String");
+    gets(myString);
+    printf("%s" , myString);
 
 
     return 0;
 }
 
 
-//user-defined section 
+//user-defined section
