1da5ea4fc0c5be366981119ed7be02bda0aac0f5
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-10 01:53:12 +0800

    Update exercise6.c

--- exercise6.c
+++ exercise6.c
@@ -1,4 +1,5 @@
 //documentation section 
+//Array Week 3,Exercise 6
 
 //pre-processor section 
 #include<stdio.h>
@@ -9,6 +10,16 @@
 //main function section 
 int main(){
     //write here your program
+    
+    int i,j;
+    int num[3][2]={{0,2},{4,6},{8,10}};
+    
+    for(int i=0; i<3; i++){
+        for (int j = 0; j < 2; j++){
+            printf("Element at num[%d][%d]: %d \n", i, j, num[i][j]);
+            }
+               
+    }
 
 
     return 0;
