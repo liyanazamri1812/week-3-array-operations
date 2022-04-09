bb8ce5f81df81bb1db5070b06ef94b2731edc94c
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-10 03:16:15 +0800

    Update exercise8.c

--- exercise8.c
+++ exercise8.c
@@ -1,18 +1,33 @@
 //documentation section 
+//Array Week 3, Exercise 8
 
 //pre-processor section 
 #include<stdio.h>
 
 //global variable section 
-
+void displayElement(int num [3][2], int i, int j);
 
 //main function section 
 int main(){
     //write here your program
-
-
+    
+    int num[3][2]= {{0,2},{4,6},{8,10}};
+    int i,j;
+    
+    for (i=0 ; i<3; i++)
+    {
+        for (j =0; j<2; j++)
+        {
+            displayElement(num,i,j); 
+        }
+    }   
+    
     return 0;
 }
 
 
 //user-defined section 
+void displayElement(int num[3][2], int i, int j)
+{
+    printf("Element at num{%d][%d]: %d\n" ,i,j,num[i][j]);
+}  
