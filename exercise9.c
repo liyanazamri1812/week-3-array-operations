a0c96dfb57974b4f8786343b6fca3627eb2cf80e
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-10 04:29:17 +0800

    Update exercise9.c

--- exercise9.c
+++ exercise9.c
@@ -1,4 +1,5 @@
 //documentation section 
+//Array Week 3, Exercise 9
 
 //pre-processor section 
 #include<stdio.h>
@@ -9,6 +10,16 @@
 //main function section 
 int main(){
     //write here your program
+    
+    int i;
+    int myArray[] = {1,2,3,4,5};
+    int* ptr;
+    
+    for (i=0; i<6; i++)
+    {
+        ptr = &myArray[i];
+        printf("Address of Element [%d] is : %d\n",i,ptr);
+    }   
 
 
     return 0;
