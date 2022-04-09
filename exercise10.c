8c39fd91c6b94977fbcb0f8799badd363968f42e
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-10 04:38:08 +0800

    Update exercise10.c

--- exercise10.c
+++ exercise10.c
@@ -1,4 +1,5 @@
 //documentation section 
+//Array Week3, Exercise 10
 
 //pre-processor section 
 #include<stdio.h>
@@ -9,6 +10,14 @@
 //main function section 
 int main(){
     //write here your program
+    
+    int i;
+    int  myVariable = 100;
+    int* ptr;
+    
+    ptr = &myVariable;
+    printf("Address for ptr is : %d\n",ptr);
+    printf("Value for ptr is : %d\n",ptr);
 
 
     return 0;
