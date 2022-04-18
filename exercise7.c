3950aeb166fbe839ab708abcebd2a0d3889b09c8
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-04-19 03:22:41 +0800

    Update exercise7.c

--- exercise7.c
+++ exercise7.c
@@ -3,12 +3,30 @@
 //pre-processor section 
 #include<stdio.h>
 
+float summationArray(float num[10])
+{
+    float sum = 0.0;
+    for ( int i=0; i<10; i++)
+    {
+        sum = sum + num[i];
+    }
+    return sum;
+}
+
 //global variable section 
 
 
 //main function section 
 int main(){
     //write here your program
+    float total;
+    float num[10]={10.0, 22.5,15.5, 32.5, 10.9, 67.9, 12.5, 11.9, 25.5, 100.0};
+
+    total = summationArray(num);
+    printf("Total : %1.f", total);
+    
+
+    
 
 
     return 0;
